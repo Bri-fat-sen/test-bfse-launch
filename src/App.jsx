77706213bd1ff6fe8a960eab90715d061f95fb5d
Bri-fat-sen <ansumanam@brifatsenenterprise.com>
2026-04-15 11:49:38 +1000

    chore: add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-background">
+      <h1 className="text-2xl font-bold">test-bfse-launch</h1>
+    </div>
+  );
+}
